a40b771143da1d9adf8e5e677da19b0a3aeb85f6
Jongsung <jos8304@hotmail.com>
2025-02-10 11:19:43 -0500

    add pages

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -1,15 +1,60 @@
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navber/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Mainpage from "./Page/MainPage/Mainpage";
+import About from "./Page/About/about";
+import Services from "./Page/Services/Services";
+import Contact from "./Page/Contact/Contact";
+import Teams from "./Page/Teams/Teams";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Mainpage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      {
+        path: "/team",
+        element: <Teams />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Footer />
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frondend/src/Page/MainPage/Mainpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Mainpage = () => {
+  return <div className="py-32">Mainpage</div>;
+};
+
+export default Mainpage;
