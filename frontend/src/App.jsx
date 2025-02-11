7b7e575365acc44d692abdb326cc463c5511f137
Jongsung <jos8304@hotmail.com>
2025-02-11 15:33:47 -0500

    add extension

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import MainPage from "./Page/MainPage/Mainpage";
-import Leadership from "./Page/Leadership/Leadership";
-import About from "./Page/About/about";
-import Board from "./Page/Board/board";
-import Services from "./Page/Services/Services";
-import Contact from "./Page/MainPage/Contact";
-import Footer from "./Components/Footer/Footer";
-import NavBar from "./Components/Navber/Navbar";
+import MainPage from "./Page/MainPage/Mainpage.jsx";
+import Leadership from "./Page/Leadership/Leadership.jsx";
+import About from "./Page/About/about.jsx";
+import Board from "./Page/Board/Board.jsx";
+import Services from "./Page/Services/Services.jsx";
+import Contact from "./Page/MainPage/Contact.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+import NavBar from "./Components/Navber/Navbar.jsx";
 
 function Layout() {
   return (
